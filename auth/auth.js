8d74f24e038a57cc71ffc53a3a83f61e35d94706
prabhakaran <prabhakarvijayan24@gmail.com>
2024-01-10 12:07:34 +0530

    auth done

--- auth/auth.js
+++ auth/auth.js
@@ -5,7 +5,18 @@ const secret= process.env.SECRET_ID
 
 const authenticate= (req,res,next)=>{
     const authheader= req.header.authorisation
-    if(authheader){
-        const token= authheader.split(" ")[1]
+    if(!authheader){
+        return res.status(401).send("no token headers")
+        
     }
+    const token= authheader.split(" ")[1]
+    jwt.verify(token,secret,(err, decoded)=>{
+        if(err){
+            res.status(401).send("token is invalid now")
+        }
+        console.log(decoded)
+        next()
+    })
 }
+
+module.exports={authenticate}
